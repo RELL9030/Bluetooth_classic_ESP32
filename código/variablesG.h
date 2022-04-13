e866cd81ac43be27478bb8ce15a53e5ca4c05bc5
Rubén Lozano <70780174+RELL9030@users.noreply.github.com>
2022-04-12 22:37:53 -0500

    Update variablesG.h

--- código/variablesG.h
+++ código/variablesG.h
@@ -2,6 +2,14 @@
 * Variables Globales y estructuras
 */
 
+#define RS 5 //PIN D19 o D5
+#define EN 17 //PIN D18 o TX2 GPIO17
+#define DB4 16 //PIN D5 o RX2 GPIO16
+#define DB5 4 //PIN D4
+#define DB6 2 //PIN D2
+#define DB7 15 //PIN D15
+
+
 LiquidCrystal lcd(RS, EN, DB4, DB5, DB6, DB7); //INICIALIZACION LCD 16x2
 BluetoothSerial SerialBT; // Bluetooth Serial object
 
